fae63916d94bb7c1dc12ec5f14f9f70327d45442
SilverCondor18 <ckraer@gmail.com>
2025-05-26 20:28:39 +0300

    Add validators to routes

--- src/routes/contacts.js
+++ src/routes/contacts.js
@@ -1,19 +1,21 @@
 import { Router } from "express";
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
 import { createContactController, deleteContactController, getAllContactsController, getContactByIdController, patchContactController, upsertContactController } from "../controllers/contacts.js";
-
+import { createContactSchema, updateContactsSchema } from "../validation/contacts.js";
+import { validateBody } from "../middlewares/validateBody.js";
+import { isValidId } from "../middlewares/isValidId.js";
 const router = Router();
 
 router.get('/contacts', ctrlWrapper(getAllContactsController));
 
-router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+router.get('/contacts/:contactId', isValidId, ctrlWrapper(getContactByIdController));
 
-router.post('/contacts', ctrlWrapper(createContactController));
+router.post('/contacts', validateBody(createContactSchema), ctrlWrapper(createContactController));
 
-router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
+router.delete('/contacts/:contactId', isValidId, ctrlWrapper(deleteContactController));
 
-router.put('/contacts/:contactId', ctrlWrapper(upsertContactController));
+router.put('/contacts/:contactId', isValidId, validateBody(updateContactsSchema), ctrlWrapper(upsertContactController));
 
-router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
+router.patch('/contacts/:contactId', isValidId, validateBody(updateContactsSchema), ctrlWrapper(patchContactController));
 
 export default router;
--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -3,7 +3,15 @@ import Joi from 'joi';
 export const createContactSchema = Joi.object({
     name: Joi.string().min(5).max(30).required(),
     phoneNumber: Joi.string().min(10).max(13).required(),
-    email: Joi.string().min(6).max(50),
     isFavourite: Joi.boolean().required(),
     contactType: Joi.string().valid("home", "personal").required()
 });
+
+export const updateContactsSchema = Joi.object({
+    name: Joi.string().min(5).max(30),
+    phoneNumber: Joi.string().min(10).max(13),
+    email: Joi.string().email(),
+    isFavourite: Joi.boolean(),
+    contactType: Joi.string().valid("home", "personal")
+});
+
