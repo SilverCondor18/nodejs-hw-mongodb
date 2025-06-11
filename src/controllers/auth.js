348d0ca6919821222267444c333703ff3a08ad0e
SilverCondor18 <ckraer@gmail.com>
2025-06-12 01:16:15 +0300

    Add password reset feature

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,5 @@
 import { ONE_DAY } from "../constants/index.js";
-import { loginUser, logoutUser, refreshUserSession, registerUser } from "../services/auth.js";
+import { loginUser, logoutUser, refreshUserSession, registerUser, requestResetToken, resetPassword } from "../services/auth.js";
 
 const setupSession = (res, session) => {
     res.cookie("refreshToken", session.refreshToken, {
@@ -61,4 +61,22 @@ export const logoutUserController = async (req, res) => {
     res.clearCookie("sessionId");
     res.clearCookie("refreshToken");
     res.status(204).send();
+};
+
+export const requestResetEmailController = async (req, res) => {
+    await requestResetToken(req.body.email);
+    res.json({
+        message: "Reset password email was successfully sent!",
+        status: 200,
+        data: {}
+    });
+};
+
+export const resetPasswordController = async (req, res) => {
+    await resetPassword(req.body);
+    res.json({
+        message: "Password was successfully reset!",
+        status: 200,
+        data: {}
+    });
 };
