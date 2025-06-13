18e7a49a1f81c0efb38eaf409559acba0f7c4581
SilverCondor18 <ckraer@gmail.com>
2025-06-14 02:10:00 +0300

    change request-reset-email to send-reset-email route

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -14,7 +14,7 @@ router.post("/refresh", ctrlWrapper(refreshUserSessionController));
 
 router.post("/logout", ctrlWrapper(logoutUserController));
 
-router.post("/request-reset-email", validateBody(requestResetEmailSchema), ctrlWrapper(requestResetEmailController));
+router.post("/send-reset-email", validateBody(requestResetEmailSchema), ctrlWrapper(requestResetEmailController));
 
 router.post("/reset-password", validateBody(resetPasswordSchema), ctrlWrapper(resetPasswordController));
 
