b1500991c0dd34ae14470b6d16cd0e41360214ca
SilverCondor18 <ckraer@gmail.com>
2025-06-17 21:54:59 +0300

    Add Google OAuth feature

--- src/services/auth.js
+++ src/services/auth.js
@@ -11,6 +11,7 @@ import { SMTP } from "../constants/index.js";
 import handlebars from "handlebars";
 import path from "node:path";
 import fs from "node:fs/promises";
+import { getFullNameFromGoogleTokenPayload, validateCode } from "../utils/googleOAuth2.js";
 
 const createSession = () => {
     const accessToken = randomBytes(30).toString("base64");
@@ -159,4 +160,31 @@ export const resetPassword = async payload => {
             password: encryptedPassword
         }
     );
+};
+
+export const loginOrSignupWithGoogle = async code => {
+    const loginTicket = await validateCode(code);
+    const payload = loginTicket.getPayload();
+    if(!payload) {
+        throw createHttpError(401, "Unauthorized");
+    }
+
+    let user = await UsersCollection.findOne({
+        email: payload.email
+    });
+    if(!user) {
+        const password = await bcrypt.hash(randomBytes(10), 10);
+        user = await UsersCollection.create({
+            email: payload.email,
+            name: getFullNameFromGoogleTokenPayload(payload),
+            password
+        });
+    }
+
+    const newSession = createSession();
+
+    return await SessionsCollection.create({
+        userId: user._id,
+        ...newSession
+    });
 };
