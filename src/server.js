dd1a884eebda7d221b822712dfe89bd815341139
SilverCondor18 <ckraer@gmail.com>
2025-06-22 18:17:08 +0300

    Add docs for other contacts routes

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ import router from "./routes/index.js";
 import { notFoundHandler, errorHandler } from "./middlewares/errorHandler.js";
 import cookieParser from "cookie-parser";
 import { UPLOAD_DIR } from './constants/index.js';
-
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 export function setupServer() {
     const app = express();
@@ -25,6 +25,8 @@ export function setupServer() {
             }
         })
     );
+    app.use("/uploads", express.static(UPLOAD_DIR));
+    app.use("/api-docs", swaggerDocs());
     app.use(router);
     app.use(/(.*)/, notFoundHandler);
     app.use(errorHandler);
